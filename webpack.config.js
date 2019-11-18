1e3d8856591f2c480db4ca4fd4219f7d8f115d56
F-y-o-d-o-r <fyodor.work@gmail.com>
2019-11-18 20:27:30 +0200

    devMode check

--- webpack.config.js
+++ webpack.config.js
@@ -6,62 +6,65 @@ const PATHS = {
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
-module.exports = {
-  entry: {
-    index: `${PATHS.src}/js/index.js`,
-    test: `${PATHS.src}/js/test.js`
-  },
-  output: {
-    path: `${PATHS.src}/dist`,
-    filename: 'js/[name].js'
-  },
-  devtool: "source-map",
-  module: {
-    rules: [{
-      test: /\.js$/,
-      include: `${PATHS.src}/js`,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
-        }
-      }
+module.exports = (env, argv) => {
+  const devMode = argv.mode !== "production";
+  return {
+    entry: {
+      index: `${PATHS.src}/js/index.js`,
+      test: `${PATHS.src}/js/test.js`
     },
-      {
-        test: /\.pug$/,
-        loader: "pug-loader",
-        options: {
-
+    output: {
+      path: `${PATHS.dist}`,
+      filename: 'js/[name].js'
+    },
+    devtool: devMode ? "source-map" : "",
+    module: {
+      rules: [{
+        test: /\.js$/,
+        include: `${PATHS.src}/js`,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
         }
       },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'src/pug/en/index.pug',
-      lang: "en",
-      title: "Index page - en",
-      description: "Index page - en",
-      canonical: "https://websitename/",
-      chunks: ['index', 'test'],
-      // css: [ "main.css" ],
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'ru/index.html',
-      template: 'src/pug/ru/index.pug',
-      lang: "ru",
-      title: "Index page - ru",
-      description: "Index page - ru",
-      canonical: "https://websitename/ru",
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'ua/index.html',
-      template: 'src/pug/ua/index.pug',
-      lang: "ua",
-      title: "Index page - ua",
-      description: "Index page - ua",
-      canonical: "https://websitename/ua",
-    })
-  ]
+        {
+          test: /\.pug$/,
+          loader: "pug-loader",
+          options: {
+            pretty: devMode
+          }
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: 'src/pug/en/index.pug',
+        lang: "en",
+        title: "Index page - en",
+        description: "Index page - en",
+        canonical: "https://websitename/",
+        chunks: ['index', 'test'],
+        // css: [ "main.css" ],
+      }),
+      new HtmlWebpackPlugin({
+        filename: 'ru/index.html',
+        template: 'src/pug/ru/index.pug',
+        lang: "ru",
+        title: "Index page - ru",
+        description: "Index page - ru",
+        canonical: "https://websitename/ru",
+      }),
+      new HtmlWebpackPlugin({
+        filename: 'ua/index.html',
+        template: 'src/pug/ua/index.pug',
+        lang: "ua",
+        title: "Index page - ua",
+        description: "Index page - ua",
+        canonical: "https://websitename/ua",
+      })
+    ]
+  }
 };
